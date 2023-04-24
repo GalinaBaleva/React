72517b3daa5ca362275cb8be1a31bf4428df290d
GalinaBaleva <baleva1990@gmail.com>
2023-04-24 08:56:29 +0200

    new

--- 03. ReactJS-Components-Deep-Dive/todo-list-app/client/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-export const Header = () => {
-    <header className="navigation-header">
-    <span className="navigation-logo">
-      <img src="./images/todo-icon.png" alt="todo-logo" />
-    </span>
-    <span className="spacer"></span>
-    <span className="navigation-description">Todo List</span>
-  </header>
-}
