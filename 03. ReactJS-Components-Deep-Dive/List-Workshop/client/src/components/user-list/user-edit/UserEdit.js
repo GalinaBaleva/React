afcaa71f65b320fbafab75308ec338af756eacdb
GalinaBaleva <baleva1990@gmail.com>
2023-05-01 00:39:14 +0200

    new

--- 03. ReactJS-Components-Deep-Dive/List-Workshop/client/src/components/user-list/user-edit/UserEdit.js
+++ 03. ReactJS-Components-Deep-Dive/List-Workshop/client/src/components/user-list/user-edit/UserEdit.js
@@ -2,16 +2,16 @@ import { UserAction } from "../UserListConstants"
 
 
 export const UserEdit = ({
-    user,
-    onClose,
+  user,
+  onClose,
 }) => {
-    return (
-        <div className="overlay">
+  return (
+    <div className="overlay">
       <div className="backdrop" onClick={onClose}></div>
       <div className="modal">
         <div className="user-container">
           <header className="headers">
-            <h2>Edit User/Add User</h2>
+            <h2>Edit User</h2>
             <button className="btn close" onClick={onClose}>
               <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="xmark"
                 className="svg-inline--fa fa-xmark" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
@@ -119,8 +119,8 @@ export const UserEdit = ({
               </div>
             </div>
             <div id="form-actions">
-              <button id="action-save" className="btn" type="submit">Save</button>
-              <button id="action-cancel" className="btn" type="button">
+              <button id="action-save" className="btn" type="submit">Edit</button>
+              <button id="action-cancel" className="btn" type="button" onClick={onClose} >
                 Cancel
               </button>
             </div>
@@ -128,5 +128,5 @@ export const UserEdit = ({
         </div>
       </div>
     </div>
-    )
+  )
 }
