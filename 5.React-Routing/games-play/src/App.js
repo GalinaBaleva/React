a5269cc6c2664d95b2f2238f9dee3266e7d06f12
GalinaBaleva <baleva1990@gmail.com>
2023-05-27 14:46:14 +0300

    Add routs and pages

--- 5.React-Routing/games-play/src/App.js
+++ 5.React-Routing/games-play/src/App.js
@@ -5,6 +5,8 @@ import Header from './components/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import CreateGame from './components/Create/CreateGame';
+import Catalog from './components/Catalog/Catalog';
 
 function App() {
   return (
@@ -16,6 +18,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/create" element={<CreateGame />} />
+          <Route path="/catalog" element={<Catalog />} />
+
         </Routes>
       </main>
     </div>
