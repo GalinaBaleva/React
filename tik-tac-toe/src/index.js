e2f8d64bae01064979491dd2c2dd56d9f396dc29
Galina <baleva1990@gmail.com>
2023-07-25 00:11:14 +0300

    separating app to components

--- tik-tac-toe/src/index.js
+++ tik-tac-toe/src/index.js
@@ -1,12 +1,15 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
+import { BrowtherRouter } from 'react-router-dom';
 
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <BrowtherRouter>
+      <App />
+    </BrowtherRouter>
   </StrictMode>
 );
